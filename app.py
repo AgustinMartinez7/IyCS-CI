b99355e0f0e5a28925e08dae4c0c15ca3163822e
AgustinMartinez7 <agustindago100@gmail.com>
2025-05-26 21:32:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 def doble(numero: int) -> int:
-    return numero * 2
+    return numero * 3
 
 @app.get("/doble")
 def obtener_doble(numero: int):
     resultado = doble(numero)
     return {"resultado": resultado}
+
