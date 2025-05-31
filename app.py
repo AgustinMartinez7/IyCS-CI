9d968f64a629fe420b1a561b483574b4b38acd92
AgustinMartinez7 <agustindago100@gmail.com>
2025-05-31 14:40:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ templates = Jinja2Templates(directory="templates")
 def doble(numero: int) -> int:
     return numero * 2
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/doble", response_class=HTMLResponse)
 def obtener_doble_html(request: Request, numero: int):
     resultado = doble(numero)
     return templates.TemplateResponse("doble.html", {
