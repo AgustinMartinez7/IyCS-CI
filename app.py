23ce74993e02b62f9204b94bded649cfcdcad7f8
AgustinMartinez7 <agustindago100@gmail.com>
2025-05-31 15:50:34 -0300

    Coming back

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI
 
 app = FastAPI()
-
-templates = Jinja2Templates(directory="templates")
 
 def doble(numero: int) -> int:
     return numero * 2
 
-@app.get("/doble", response_class=HTMLResponse)
-def obtener_doble_html(request: Request, numero: int):
+@app.get("/doble")
+def obtener_doble(numero: int):
     resultado = doble(numero)
-    return templates.TemplateResponse("doble.html", {
-        "request": request,
-        "numero": numero,
-        "resultado": resultado
-    })
+    return {"resultado": resultado}
+
