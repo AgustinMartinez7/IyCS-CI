19bb24ebf90cc67ec5f16fd557763ce090f018bf
AgustinMartinez7 <agustindago100@gmail.com>
2025-06-03 14:03:16 -0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,6 +1,15 @@
+from fastapi.testclient import TestClient
 from app import doble
+from app import app
+
+client = TestClient(app)
 
 def test_doble():
     assert doble(2) == 4
     assert doble(0) == 0
     assert doble(-3) == -6
+
+def test_obtener_doble():
+    response = client.get("/doble?numero=5")
+    assert response.status_code == 200
+    assert response.json() == {"resultado": 10}
