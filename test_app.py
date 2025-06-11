1a21cce4fa206634e86da73157475e59de31b899
Agustin Martinez <agustindago100@gmail.com>
2025-06-10 23:11:39 -0300

    Delete files

--- test_app.py
+++ test_app.py
@@ -1,19 +1,6 @@
-from fastapi.testclient import TestClient
-from app import doble, app
-
-client = TestClient(app)
+from app import doble
 
 def test_doble():
     assert doble(2) == 4
     assert doble(0) == 0
     assert doble(-3) == -6
-
-def test_obtener_doble():
-    response = client.get("/doble?numero=5")
-    assert response.status_code == 200
-    assert response.json() == {"resultado": 10}
-
-def test_index():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == "Taller CI/CD - Agustin Martinez"
